8c74b9a006cb90edf690e7444e315410078083ba
AldiZulfikar <aldizulfikar04@gmail.com>
2022-07-15 16:12:22 +0700

    carousel

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -5,7 +5,7 @@
     <header class="container-fluid position-relative p-0">
         <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
           <div class="carousel-inner">
-            <div class="carousel-item active">
+            <div class="active">
               <img src="{{asset('/frontend-assets/assets/img/Home.webp')}}" class="w-100" alt="...">
               <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                 <div class="p-3" style="max-width: 900px">
@@ -23,7 +23,7 @@
                 </div>
 						  </div> 
             </div>
-            <div class="carousel-item">
+            <div class="">
               <img src="{{asset('/frontend-assets/assets/img/Home.webp')}}" class="w-100" alt="...">
               <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                 <div class="p-3" style="max-width: 900px">
