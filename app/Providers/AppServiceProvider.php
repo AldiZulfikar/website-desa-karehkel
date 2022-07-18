1854da709cdad4b374e5bbb15cffbbd1cf77d609
Aldi Zulfikar <79120740+AldiZulfikar@users.noreply.github.com>
2022-07-19 01:52:54 +0700

    Heroku Https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use \Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,8 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if ($this->app->environment('production')) {
-           URL::forceScheme('https');
+        If (env('APP_ENV') !== 'local') {
+            $this->app['request']->server->set('HTTPS', true);
         }
+
+        Schema::defaultStringLength(191);
     }
 }
